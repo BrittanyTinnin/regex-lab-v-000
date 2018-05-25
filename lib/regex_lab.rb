22845272c3d9586218da2129e7aaad18ac25fea5
Brittany Tinnin <bntinnin7@gmail.com>
2018-05-25 03:10:51 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,5 +15,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.scan(/\(?\d\d\d)?\s?\d\d\d-?\s?\d\d\d\d/).any? ? true : false
+  phone.scan(/\(?\d\d\d\)?\s?\d\d\d-?\s?\d\d\d\d/).any? ? true : false
 end
