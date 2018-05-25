36bee411a95de586a866e921fa22fabbf8704877
Brittany Tinnin <bntinnin7@gmail.com>
2018-05-25 02:42:53 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.scan(/^[A-Z].*[a-zA-z].$/)
+  text.scan(/^[A-Z].*[a-zA-z][.]$/)
 end
 
 def valid_phone_number?(phone)
